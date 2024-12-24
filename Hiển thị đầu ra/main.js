90471ba79c6cf447490b673b836ffb5d4d223b68
huy-bui27 <huy2712az@gmail.com>
2024-12-24 21:09:42 +0700

    first commit

---
+++ Hiển thị đầu ra/main.js
@@ -0,0 +1,9 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt(" Nhập chiều rộng:");
+inputHeight = prompt(" Nhạp chiều dài:");
+let width= parseInt(inputWidth);
+let height= parseInt(inputHeight);
+document.write("Chiều rộng:" + width + "<br>");
+// document.write("Chiều dài: " + height + "<br>");
+// document.write("The area is: " + (height*width));
